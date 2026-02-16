751f7375e13a4b820dadb3b204f5b0b44d910bf6
sbfcorgs <sbfcorgs@gmail.com>
2026-02-16 15:52:58 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,3 +27,11 @@ window.addEventListener('scroll', () => {
     }
   });
 });
+/* ================= SCROLL PROGRESS BAR ================= */
+window.addEventListener('scroll', () => {
+  const scrollTop = document.documentElement.scrollTop;
+  const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const progress = (scrollTop / scrollHeight) * 100;
+
+  document.getElementById('progressBar').style.width = progress + "%";
+});
